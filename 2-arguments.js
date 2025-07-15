ca3f709c80eff1d43fc4f70668fab532e3eb5dfd
yasmine Bebars <yasminebeb@hotmail.com>
2025-07-15 13:27:50 +0300

    2-arguments

---
+++ 2-arguments.js
@@ -0,0 +1,19 @@
+const mypass ='ert'
+
+
+if (mypass.length > 6  &&  mypass.includes ("@"))
+{
+console.log('No argument');
+
+}
+
+else if (mypass.length > 6 )
+{
+console.log('Argument found');
+
+}
+else (mypass.length < 6 )
+{
+console.log('Argument found2');
+
+}
