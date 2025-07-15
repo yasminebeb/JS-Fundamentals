46481cec859f0e559e207c7363da69f48f78f953
yasmine Bebars <yasminebeb@hotmail.com>
2025-07-15 14:20:42 +0300

    2-arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,19 +1,29 @@
-const mypass ='ert'
+// const mypass ='ert'
 
 
-if (mypass.length > 6  &&  mypass.includes ("@"))
-{
-console.log('No argument');
+// if (mypass.length > 6  &&  mypass.includes ("@"))
+// {
+// console.log('No argument');
 
-}
+// }
 
-else if (mypass.length > 6 )
-{
-console.log('Argument found');
+// else if (mypass.length > 6 )
+// {
+// console.log('Argument found');
 
-}
-else (mypass.length < 6 )
-{
-console.log('Argument found2');
+// }
+// else
+// {
+// console.log('Argument found');
 
+// }
+
+const args = process.argv.length - 2;
+
+if (args === 0) {
+  console.log("No argument");
+} else if (args === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
