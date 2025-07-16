fee939bcf5dbce48c4be7d09db34db79befac2f3
yasmine Bebars <yasminebeb@hotmail.com>
2025-07-16 10:45:55 +0300

    2-arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -18,11 +18,11 @@
 
 // }
 
-const args = process.argv.length - 2;
+const args = "gfjh";
 
-if (args === 0) {
+if (args.length === 0) {
   console.log("No argument");
-} else if (args === 1) {
+} else if (args.length === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
